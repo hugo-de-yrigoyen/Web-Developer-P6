02bde9e5c8623e789cb5a2a9d5fb975ab317b295
Hugo de Yrigoyen <hugo.deyrigoyen@gmail.com>
2023-02-13 10:32:44 +0100

    Comment

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-//Module mongoose pour vérifier que l'email n'est pas déjà utilisé à la création d'un compte
+//Plugin mongoose pour vérifier que l'email n'est pas déjà utilisé à la création d'un compte
 const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
